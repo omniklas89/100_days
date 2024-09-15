bb3de3a01795b4f1f18b508e5932feb33a47e9b0
omniklas89 <klasnicivan.89@gmail.com>
2024-09-15 12:25:00 +0200

    Day_1_done

--- day_1/main.py
+++ day_1/main.py
@@ -1,3 +1,7 @@
-print("HELLO WORLD!")
-print("adding a branch ... ?")
-print("back to main?")
+#input vs print functions
+print("Welcome to Band bname Generator!")
+
+city = input("Which city <re you from?\n")
+pet = input("What is your pet's name?\n")
+
+print("Your Band name can be: " + city + " " + pet)

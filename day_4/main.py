df22d80bd5c2eb556986f2f536e2162d749fd3dd
omniklas89 <klasnicivan.89@gmail.com>
2024-09-21 20:58:17 +0200

    day_4_part1

---
+++ day_4/main.py
@@ -0,0 +1,33 @@
+import random
+import my_module as myne
+
+# rand_int = random.randint(2,99)
+# print(rand_int)
+
+# random_number = random.random() * 10
+# print(round(random_number,2))
+
+# number = random.randint(0,1)
+
+# if number == 0:
+#     print("Heads!")
+# else: 
+#     print("Tails")
+
+# list_1 = ["item1","item2"]
+
+# list_1.append("item3")
+# list_1.extend(["item6","item7"])
+# list_1.pop(3)
+
+# print(list_1)
+
+
+friends = ["Ivan","Ana","Marko","Viktoria","Bro","Veky"]
+
+# random_friend = random.choice(friends)
+# print(random_friend)
+
+# random_index = random.randint(0,4)
+# print(friends[random_index])
+
